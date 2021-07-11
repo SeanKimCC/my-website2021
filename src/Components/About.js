219b790c65bffe9f3f3ada9032b35b8e501dc3d4
SeanKimCC <sswkim@uwaterloo.ca>
2021-07-11 02:59:13 -0400

    removed phone number and resume to prevent rando's spamming me with YOU WON A FREE VACATIONgit add . Calls

--- src/Components/About.js
+++ src/Components/About.js
@@ -33,14 +33,8 @@ class About extends Component {
 						   <span>{name}</span><br />
 						   <span>{city}
                </span><br />
-						   <span>{phone}</span><br />
                      <span>{email}</span>
 					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href='/resume-2020-04-29.pdf' className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
                </div>
             </div>
          </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "UA-159290967-1"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
